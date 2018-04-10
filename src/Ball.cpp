79664bbef4a15bfc66afb67999af64ccd0d0e603
lchaulan <l.chaulan@gmail.com>
2018-04-09 22:37:12 -0400

    cup & ball class update

--- src/Ball.cpp
+++ src/Ball.cpp
@@ -4,4 +4,3 @@
 
 #include "Ball.h"
 
-Ball::Ball() {}
--- src/Cup.h
+++ src/Cup.h
@@ -12,12 +12,64 @@
 class Cup {
 private:
     int ID;
-    Vec2i position;
-    bool isActive;
+    Vec2i position; //position du centre de la cup
+    bool onTable;
+    int rayon; //en cm
+    int hauteur; //en cm
+public:
+
+    /** getters
+     *
+     */
+    int getID() const {
+        return ID;
+    }
+
+    const Vec2i &getPosition() const {
+        return position;
+    }
+
+    bool isOnTable() const {
+        return onTable;
+    }
+
+    int getRayon() const {
+        return rayon;
+    }
+
+    int getHauteur() const {
+        return hauteur;
+    }
+
+    /** setters
+     *
+     */
+    void setID(int ID) {
+        Cup::ID = ID;
+    }
+
+    void setPosition(const Vec2i &position) {
+        Cup::position = position;
+    }
+
+    void setOnTable(bool onTable) {
+        Cup::onTable = onTable;
+    }
+
+    void setRayon(int rayon) {
+        Cup::rayon = rayon;
+    }
+
+    void setHauteur(int hauteur) {
+        Cup::hauteur = hauteur;
+    }
+
 public:
     Cup() {}
 
-    Cup(int ID, const Vec2i &position) : ID(ID), position(position) {}
+    Cup(int ID, const Vec2i &position, int hauteur, int rayon) : ID(ID), position(position), hauteur(hauteur), rayon(rayon) {}
+
+    Cup(int ID, const Vec2i &position) : ID(ID), position(position) {hauteur = 12; rayon = 10;}
 
     Cup(int ID) : ID(ID) {/*std::cout << "Cups " << Cup::ID << " crée ! :)" << std::endl;*/}
 
