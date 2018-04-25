d4ee06149ea1c96aa16d228329bec05bf03510c5
mac <harifnajwa@gmailcom>
2018-04-25 17:10:23 -0400

    Adding comments

--- main.h
+++ main.h
@@ -25,13 +25,6 @@
 #include "Sample.h"
 #include "Player.h"
 
-
-
-/// Moving main example.
-/// This sample demonstrates:
-///     - Adding Sprite elements to the UI
-///     - Storing custom data (sprite velocity) inside UI elements
-///     - Handling frame update events in which the main are moved
 class main : public Sample
 {
     // Enable type information.
@@ -55,28 +48,42 @@ protected:
     }
 
 private:
-    /// Construct the main
+    /** Create the main page of the game : Cups , table, score window, background */
     void InitBoardGame();
-    /// Move the main using the delta time step given.
-    void Movemain(float timeStep);
+
     /// Subscribe to application-wide logic update events.
     void SubscribeToEvents();
-    /// Handle the logic update event.
-    void HandleUpdate(StringHash eventType, VariantMap& eventData);
+
+
+    /** Create the welcome page of the game : Background, play window and button, title */
     void InitWelcomePage();
+    /** Create the draggable ping pong ball */
     void CreatedraggableBall();
+    /** Create a return button for the main page of the game */
     void CreateReturnButton();
 
+    /// Events handlers
+    /** Handler of screen update event */
+    void HandleUpdate(StringHash eventType, VariantMap& eventData);
+    /** Handler of beginning of the pinpong ball drag */
     void HandleDragBegin(StringHash eventType, VariantMap& eventData);
-    /// Handle drag move for the fish button.
+    /** Handler of the drag move of the pinpong ball */
     void HandleDragMove(StringHash eventType, VariantMap& eventData);
-    /// Handle drag end for the fish button.
+    /** Handler of the drag end of the pinpong ball */
     void HandleDragEnd(StringHash eventType, VariantMap& eventData);
+    /** Handler of the return button of the main page */
     void HandleReturnPressed(StringHash eventType, VariantMap& eventData);
+    /** Handler of the play button of the welcome page */
     void HandlePlayPressed(StringHash eventType, VariantMap& eventData);
+    /** Handler of the mouse click to move the ball at the mouse position */
     void HandleMouse(StringHash eventType, VariantMap& eventData);
 
-
+    /**
+     * Calculate the rotation of the movement
+     * @param initPos2 : initial position of the ball (pix)
+     * @param endPos2 : end position of the ball (pix)
+     * @return : angle of the movement
+     */
     double GetRotation(IntVector2 initPos2, IntVector2 endPos2){
         double sizeVec = sqrt((initPos2.x_ - endPos2.x_)*(initPos2.x_ - endPos2.x_) + (initPos2.y_ - endPos2.y_)*(initPos2.y_ - endPos2.y_));
         //  std::cout << "Taille vecteur : " << sizeVec << std::endl;
@@ -84,16 +91,35 @@ private:
         std::cout << "Rotation angle in radians : " << rotation << std::endl;
         return rotation;
     }
+
+    /**
+     * Calculate the speed of the movement
+     * @param initPos2 : initial position of the ball (pix)
+     * @param endPos2 : end position of the ball (pix)
+     * @return : speed of the movement
+     * TODO : Normaliser entre 2 valeurs (demander aux archi)
+     */
     double GetSpeed(IntVector2 initPos2, IntVector2 endPos2){
         double distance = sqrt((initPos2.x_  - endPos2.x_)*(initPos2.x_  - endPos2.x_) + (initPos2.y_  - endPos2.y_)*(initPos2.y_  - endPos2.y_));
-        double distance_cm = distance * 0.027;//64583333;
+        double distance_cm = distance;//64583333;
         clock_t endTime = clock();
         double timePassed_sec = (endTime - startTime) / ((double)CLOCKS_PER_SEC/10);
         std::cout << "Time passed : " << timePassed_sec << std::endl;
         double speed = distance_cm/timePassed_sec;
         return speed;
     };
+
+    /**
+     * Translate the position from pixels to cm
+     * @param initPos : initial position (pix)
+     * @return : initial position (cm)
+     */
     IntVector3 GetInitPosCm(IntVector2 initPos);
+
+    /**
+     * Modify the text window to announce score
+     * @param cupScored
+     */
     void ThrowResult(int cupScored);
 
     /// Vector to store the main for iterating through them.
